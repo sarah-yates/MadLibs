9a68869c57235a076095a8910baece04524bb0b0
sarahafreeze <sarahafreeze@gmail.com>
2019-12-11 18:29:51 -0600

    Third Commit

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Give me some information and I'll give you a funny remix!");
+        System.out.println("Give me some information and I'll give you a funny remix of a Christmas Carol!");
         System.out.print("Give me a noun: ");
         String firstNoun = readString();
         System.out.print("Now give me an adjective: ");
@@ -18,7 +18,10 @@ public class Main {
         System.out.println();
         System.out.println("--------------------------------------------------------------");
         System.out.println();
-
+        System.out.println("Oh the " + firstNoun + " outside is " + firstAdjective + ",");
+        System.out.println("But the " + secondNoun + " is so " + secondAdjective + ",");
+        System.out.println("And since we've no place to " + verb + ",");
+        System.out.println("Let it snow, let it snow, let it snow.");
     }
     private static String readString() {
         Scanner input = new Scanner(System.in);
