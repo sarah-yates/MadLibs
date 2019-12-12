fb6d6a23f868a288c93af35c653da49f1ce825e0
sarahafreeze <sarahafreeze@gmail.com>
2019-12-11 18:26:15 -0600

    Second Commmit

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,21 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Give me some information and I'll give you a funny remix!");
-        System.out.println("Give me a noun:");
+        System.out.print("Give me a noun: ");
         String firstNoun = readString();
-        System.out.println("Now give me an adjective:");
+        System.out.print("Now give me an adjective: ");
         String firstAdjective = readString();
-        System.out.println("Now give me a plural noun:");
+        System.out.print("Now give me another noun: ");
+        String secondNoun = readString();
+        System.out.print("Now give me another adjective: ");
+        String secondAdjective = readString();
+        System.out.print("Last one! Give me a verb: ");
+        String verb = readString();
+        System.out.println("Okay, get ready!");
+        System.out.println();
+        System.out.println("--------------------------------------------------------------");
+        System.out.println();
+
     }
     private static String readString() {
         Scanner input = new Scanner(System.in);
